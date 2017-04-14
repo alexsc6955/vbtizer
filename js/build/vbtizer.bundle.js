806b0288afb70eee997cbb34afd639b148e572dc
rincorpes <rincorpes@gmail.com>
2017-04-13 22:11:01 -0400

    testing javascript

--- js/build/vbtizer.bundle.js
+++ js/build/vbtizer.bundle.js
@@ -73,7 +73,32 @@
 "use strict";
 
 
-alert('hello world');
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Test = function () {
+	function Test(message) {
+		_classCallCheck(this, Test);
+
+		this.message = message;
+	}
+
+	_createClass(Test, [{
+		key: 'getMessage',
+		get: function get() {
+			return this.message;
+		}
+	}]);
+
+	return Test;
+}();
+
+;
+
+var test = new Test('Hello World');
+
+console.log(test.getMessage);
 
 /***/ })
 /******/ ]);
--- js/vbtizer.js
+++ js/vbtizer.js
@@ -1,1 +1,15 @@
-alert('hello world');
+class Test {
+
+	constructor(message) {
+		this.message = message;
+	};
+
+	get getMessage() {
+		return this.message;
+	}
+
+};
+
+let test = new Test('Hello World');
+
+console.log(test.getMessage);
